ef57d4703af48711c20647ab5665c39c757027bb
SAGASA_main <sagasa.ota@gmail.com>
2022-10-21 08:48:08 +0900

    ローカル動作版を作成

---
+++ src/components/root.tsx
@@ -0,0 +1,21 @@
+import React, {
+    useEffect,
+    useState,
+    useRef,
+    useLayoutEffect,
+    useCallback,
+    useContext,
+  } from "react";
+  
+  import * as vec2 from "../vec2";
+  import "../bookmark";
+  import bookmark from "../bookmark";
+
+
+  const AppRoot: React.FC = () => {
+    return (<div>
+        
+    </div>)
+  }
+
+  export default AppRoot
